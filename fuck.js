d298d65db21bfc5cff506aeed7c6534a0e29cbb8
marzuk-zarir <business.marzukzarir@gmail.com>
2023-01-13 22:50:43 +0600

    fuck

--- fuck.js
+++ fuck.js
@@ -2,7 +2,7 @@ const fs = require('fs/promises')
 const path = require('path')
 
 ;(async () => {
-    for (let i = 0; i < 500; i++) {
+    for (let i = 0; i < 10000; i++) {
         await fs.writeFile(
             path.resolve(__dirname, `fuck-${Math.random()}-${new Date().getTime()}-${i}`),
             `fuck ${i}`
